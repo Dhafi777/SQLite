6c567c768ad351227ee0ea5919111fb5e6c809f4
Dhafi777 <ininama.404@gmail.com>
2024-11-18 15:12:29 +0700

    Second Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
 }
-
 android {
     namespace = "com.dhafi.sqliteexample"
     compileSdk = 35
